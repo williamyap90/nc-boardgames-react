83b20aaa2a93f24faab9d8eadeb97243ddefa83e
williamyap90 <william.yap90@gmail.com>
2021-08-27 17:02:22 +0100

    add comments button and functionality

--- nc-boardgames/src/Api.js
+++ nc-boardgames/src/Api.js
@@ -4,24 +4,28 @@ const api = axios.create({
   baseURL: "https://nc-boardgames.herokuapp.com/api",
 });
 
-export const getReviews = async () => {
-  const { data } = await api.get("/reviews");
-  return data.result.reviews;
-};
-
 export const getCategories = async () => {
   const { data } = await api.get("/categories");
   return data.result;
 };
 
-export const getUsers = async () => {
-  const { data } = await api.get("/users");
-  return data.users;
+export const getReviews = async () => {
+  const { data } = await api.get("/reviews");
+  return data.result.reviews;
 };
-
 export const getSingleReview = async (review_id) => {
   const { data } = await api.get(`/reviews/${review_id}`);
   return data.review[0];
+};
+
+export const getCommentsByReviewId = async (review_id) => {
+  const { data } = await api.get(`reviews/${review_id}/comments`);
+  return data.comments;
+};
+
+export const getUsers = async () => {
+  const { data } = await api.get("/users");
+  return data.users;
 };
 
 export const getSingleUser = async (username) => {
--- nc-boardgames/src/components/SingleReview.jsx
+++ nc-boardgames/src/components/SingleReview.jsx
@@ -1,16 +1,26 @@
 import { useEffect, useState } from "react";
-import { getSingleReview } from "../Api";
+import { getSingleReview, getCommentsByReviewId } from "../Api";
 import { useParams, Link } from "react-router-dom";
 
 const SingleReview = () => {
-  const [singleReview, setSingleReview] = useState({});
   const { review_id } = useParams();
+  const [singleReview, setSingleReview] = useState({});
+  const [comments, setComments] = useState([]);
+  const [showComments, setShowComments] = useState(false);
 
   useEffect(() => {
     getSingleReview(review_id).then((data) => {
       setSingleReview(data);
     });
-  }, [review_id]);
+  }, [review_id, showComments]);
+
+  const getComments = (review_id) => {
+    getCommentsByReviewId(review_id).then((data) => {
+      setComments(data);
+    });
+    //create a button in review, onClick execute getComments & show comments
+    //set button true or false to show or hide comments, conditional to display?
+  };
 
   return (
     <section>
@@ -31,9 +41,32 @@ const SingleReview = () => {
           <p>Comment count: {singleReview.comment_count}</p>
           <p>Review ID: {singleReview.review_id}</p>
           <p>Votes: {singleReview.votes}</p>
+          <button
+            className="reviews__button"
+            onClick={() => {
+              getComments(singleReview.review_id);
+              showComments === true
+                ? setShowComments(false)
+                : setShowComments(true);
+            }}
+          >
+            View comments
+          </button>
           <Link to="/reviews">
             <button className="reviews__button">Go back</button>
           </Link>
+          {showComments &&
+            comments.map((comment) => {
+              return (
+                <div className="comments__container" key={comment.comment_id}>
+                  <p>Comment ID: {comment.comment_id}</p>
+                  <p>Author: {comment.author}</p>
+                  <p>Comment: {comment.body}</p>
+                  <p>Created at: {comment.created_at}</p>
+                  <p>Votes: {comment.votes}</p>
+                </div>
+              );
+            })}
         </li>
       </ul>
     </section>
