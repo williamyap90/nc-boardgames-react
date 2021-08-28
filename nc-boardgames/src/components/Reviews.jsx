55e1a1a19381ad5ee88f6e4109ec019ac0b6c9ca
williamyap90 <william.yap90@gmail.com>
2021-08-28 11:29:29 +0100

    styled cards for animation

--- nc-boardgames/src/components/Reviews.jsx
+++ nc-boardgames/src/components/Reviews.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { getReviews } from "../Api";
 import { Link } from "react-router-dom";
-import { Button, Icon, Divider } from "semantic-ui-react";
+import { Button, Icon, Divider, Reveal, Image } from "semantic-ui-react";
 
 const Reviews = () => {
   const [reviews, setReviews] = useState([]);
@@ -13,8 +13,8 @@ const Reviews = () => {
   }, []);
 
   return (
-    <section>
-      <Divider horizontal class="page__divider">
+    <section className="section__body section__body-reviews">
+      <Divider horizontal className="page__divider">
         Reviews
       </Divider>
       <ul className="reviews__container">
@@ -22,30 +22,35 @@ const Reviews = () => {
           return (
             <li className="reviews__item" key={review.review_id}>
               <h3 className="reviews__item-title">{review.title}</h3>
-              <img
-                className="reviews__image"
-                src={review.review_img_url}
-                alt={review.review_title}
-              />
-              <p>User: {review.owner}</p>
-              <p>Category: {review.category}</p>
-              <p>Created at: {review.created_at}</p>
-              <p>Comment count: {review.comment_count}</p>
-              <p>Review ID: {review.review_id}</p>
-              <p>Votes: {review.votes}</p>
-              <Link to={`/reviews/${review.review_id}`}>
-                <Button animated className="reviews__button">
-                  <Button.Content visible>View more</Button.Content>
-                  <Button.Content hidden>
-                    <Icon name="arrow right" />
-                  </Button.Content>
-                </Button>
-              </Link>
+              <Reveal animated="move">
+                <Reveal.Content visible class="reviews__item">
+                  <Image src={review.review_img_url} size="massive" />
+                </Reveal.Content>
+                <Reveal.Content hidden>
+                  <p>{review.review_title}</p>
+                  <p>User: {review.owner}</p>
+                  <p>Category: {review.category}</p>
+                  <p>Created at: {review.created_at}</p>
+                  <p>Comment count: {review.comment_count}</p>
+                  <p>Review ID: {review.review_id}</p>
+                  <p>Votes: {review.votes}</p>
+                  <Link to={`/reviews/${review.review_id}`}>
+                    <Button animated className="reviews__button">
+                      <Button.Content visible>View more</Button.Content>
+                      <Button.Content hidden>
+                        <Icon name="arrow right" />
+                      </Button.Content>
+                    </Button>
+                  </Link>
+                </Reveal.Content>
+              </Reveal>
             </li>
           );
         })}
       </ul>
-      <p> Previous - Showing 1-10 of 24 - Next</p>
+      <p className="reviews__pagination">
+        Previous - Showing 1-10 of 24 - Next
+      </p>
     </section>
   );
 };
