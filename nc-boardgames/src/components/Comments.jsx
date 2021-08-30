244d7ac43442ba1799b7f261521dd58688904093
williamyap90 <william.yap90@gmail.com>
2021-08-30 16:03:23 +0100

    add classNames & icons

--- nc-boardgames/src/components/Comments.jsx
+++ nc-boardgames/src/components/Comments.jsx
@@ -1,11 +1,25 @@
+import { Icon } from "semantic-ui-react";
+
 const displayComment = (comment) => {
   return (
     <div className="comments__container" key={comment.comment_id}>
-      <p>Comment ID: {comment.comment_id}</p>
-      <p>Author: {comment.author}</p>
-      <p>Comment: {comment.body}</p>
-      <p>Created at: {comment.created_at}</p>
-      <p>Votes: {comment.votes}</p>
+      <p className="comments__body">{comment.body}</p>
+      <p className="comments__author">{comment.author}</p>
+      <p className="comments__created">{comment.created_at}</p>
+
+      <Icon
+        color="grey"
+        name="caret up"
+        size="large"
+        className="comments__upvote"
+      />
+      <p className="comments__votes">{comment.votes} Votes</p>
+      <Icon
+        color="grey"
+        name="caret down"
+        size="large"
+        className="comments__downvote"
+      />
     </div>
   );
 };
