1f47e761dc7804101c621bf433e45734da81a3a5
williamyap90 <william.yap90@gmail.com>
2021-08-30 07:20:35 +0100

    moved filters from left sidebar to top, & redone styling

--- nc-boardgames/src/components/Filters.jsx
+++ nc-boardgames/src/components/Filters.jsx
@@ -1,4 +1,3 @@
-import { Divider } from "semantic-ui-react";
 import { useEffect } from "react";
 import { getCategories } from "../Api";
 
@@ -11,42 +10,52 @@ const Filters = ({ categories, setCategories }) => {
 
   return (
     <section className="filters__container">
-      <Divider horizontal className="filters__header">
-        Filters:
-      </Divider>
-      <label htmlFor="category">Category:</label>
-      <select name="category" id="category">
-        <option value="">Select a category</option>
-        {categories.map((category) => {
-          return (
-            <option
-              value={category.slug}
-              className="category__filter-option"
-              key={category.slug}
-            >
-              {category.slug}
-            </option>
-          );
-        })}
-      </select>
+      <div className="filters__header">Filters:</div>
+      <div className="filters__main">
+        <label htmlFor="category">Category:</label>
+        <select name="category" id="category">
+          <option value="">Select a category</option>
+          {categories.map((category) => {
+            return (
+              <option
+                value={category.slug}
+                className="category__filter-option"
+                key={category.slug}
+              >
+                {category.slug}
+              </option>
+            );
+          })}
+        </select>
 
-      <label htmlFor="sort_by">Sort by:</label>
-      <select name="sort_by" id="sort_by">
-        <option value="">Select sort by</option>
-        <option value="review">Review ID</option>
-        <option value="title">Title</option>
-        <option value="votes">Votes</option>
-        <option value="created_at">Created at</option>
-      </select>
+        <label htmlFor="sort_by">Sort by:</label>
+        <select name="sort_by" id="sort_by">
+          <option value="">Select sort by</option>
+          <option value="review">Review ID</option>
+          <option value="title">Title</option>
+          <option value="votes">Votes</option>
+          <option value="created_at">Created at</option>
+        </select>
 
-      <label htmlFor="order">Order:</label>
-      <select name="order" id="order">
-        <option value="">Select order</option>
-        <option value="asc">Ascending</option>
-        <option value="desc">Descending</option>
-      </select>
+        <label htmlFor="order">Order:</label>
+        <select name="order" id="order">
+          <option value="">Select order</option>
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
 
-      <label htmlFor="page">Results per page: 10</label>
+        <label htmlFor="limit">Results per page: </label>
+        <select name="limit" id="limit">
+          <option value="">#</option>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+          <option value="20">20</option>
+          <option value="25">25</option>
+          <option value="50">50</option>
+        </select>
+      </div>
+      <div className="filters__separator"></div>
     </section>
   );
 };
