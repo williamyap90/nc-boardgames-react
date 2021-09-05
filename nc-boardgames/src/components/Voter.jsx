f7e609c58a7942a36a79df65149589692da9bad4
william yap <william.ap90@gmail.com>
2021-09-05 12:01:26 +0100

    changed updateVotedObj & disabled checks to disable only upvote OR downvote instead of both on click of either one

--- nc-boardgames/src/components/Voter.jsx
+++ nc-boardgames/src/components/Voter.jsx
@@ -8,12 +8,13 @@ const Voter = ({
   votedObj,
   setVotedObj,
 }) => {
-  const changeVotes = (num, id) => {
+  const changeVotes = (num, id, voteType) => {
     setState((current) => {
       if (!Array.isArray(current)) {
         const newReview = { ...current };
         if (newReview.review_id === current_id) {
           newReview.votes += num;
+          updateVotedObj("review_id", current_id, voteType);
         }
         return newReview;
       }
@@ -25,12 +26,12 @@ const Voter = ({
         if (item.hasOwnProperty("review_id")) {
           if (item.review_id === current_id) {
             item.votes += num;
-            updateVotedObj("review_id", current_id);
+            updateVotedObj("review_id", current_id, voteType);
           }
         } else if (item.hasOwnProperty("comment_id")) {
           if (item.comment_id === current_id) {
             item.votes += num;
-            updateVotedObj("comment_id", current_id);
+            updateVotedObj("comment_id", current_id, voteType);
           }
         }
       });
@@ -41,16 +42,13 @@ const Voter = ({
     });
   };
 
-  const updateVotedObj = (typeOfId, currentId) => {
+  const updateVotedObj = (typeOfId, currentId, voteType) => {
     setVotedObj((currentObj) => {
       const newCurrentObj = { ...currentObj };
-      newCurrentObj[`${typeOfId} ${currentId}`] = true;
+      newCurrentObj[`${voteType} ${typeOfId} ${currentId}`] = true;
       return newCurrentObj;
     });
   };
-
-  console.log(current_id, "<<currentId");
-  console.log(votedObj);
 
   return (
     <>
@@ -60,11 +58,13 @@ const Voter = ({
         name="chevron up"
         size="large"
         disabled={
-          (current_id && votedObj.hasOwnProperty(`review_id ${current_id}`)) ||
-          (current_id && votedObj.hasOwnProperty(`comment_id ${current_id}`))
+          (current_id &&
+            votedObj.hasOwnProperty(`upvote review_id ${current_id}`)) ||
+          (current_id &&
+            votedObj.hasOwnProperty(`upvote comment_id ${current_id}`))
         }
         onClick={() => {
-          changeVotes(1, current_id);
+          changeVotes(1, current_id, "upvote");
         }}
       />
       <p className="reviews__votes">{votes} Votes</p>
@@ -74,11 +74,13 @@ const Voter = ({
         name="chevron down"
         size="large"
         disabled={
-          (current_id && votedObj.hasOwnProperty(`review_id ${current_id}`)) ||
-          (current_id && votedObj.hasOwnProperty(`comment_id ${current_id}`))
+          (current_id &&
+            votedObj.hasOwnProperty(`downvote review_id ${current_id}`)) ||
+          (current_id &&
+            votedObj.hasOwnProperty(`downvote comment_id ${current_id}`))
         }
         onClick={() => {
-          changeVotes(-1, current_id);
+          changeVotes(-1, current_id, "downvote");
         }}
       />
     </>
